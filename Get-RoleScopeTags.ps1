8e04f95575155c89fcb108fdbbd0cc8b57336990
patryk.podlas <patryk.podlas@claranet.com>
2023-11-28 16:01:02 +0000

    upload

---
+++ Get-RoleScopeTags.ps1
@@ -0,0 +1,49 @@
+Function Get-RoleScopeTags {
+    param
+    (
+        [Array]$IDs,
+        $ExportPath = "$env:TEMP\deviceManagement\deviceAndAppManagementRoleAssignment"
+    )
+
+    # Initialise array
+    $dataArray = @()
+
+    # Process
+    foreach ($ID in $IDs) {
+        try {
+            # Get
+            $URI = "https://graph.microsoft.com/beta/deviceManagement/roleAssignments('$ID')?`$expand=microsoft.graph.deviceAndAppManagementRoleAssignment/roleScopeTags"
+            $request = (Invoke-MgGraphRequest -Uri $URI -Method GET)
+
+            # Sort
+            $sortedRequest = foreach ($item in $request) {
+                Format-HashtableRecursively -Hashtable $item
+            }
+
+            # Process each
+            foreach ($item in $sortedRequest) {
+                Write-Verbose "Item: $($item.description)"
+                $jsonContent = $item | ConvertTo-Json -Depth 20
+                $fileName = $item.description -replace '[\<\>\:"/\\\|\?\*]', "_"
+
+                # Create a hashtable for each file's data
+                $fileData = @{
+                    FileName   = "$fileName.json"
+                    Content    = $jsonContent
+                    ExportPath = $exportPath
+                }
+
+                # Add the hashtable to the array
+                $dataArray += $fileData
+            }
+        }
+
+        catch {
+            Write-Error "An error occurred: $($_.Exception.Message)"
+        }
+
+    }
+
+    return $dataArray
+
+}
